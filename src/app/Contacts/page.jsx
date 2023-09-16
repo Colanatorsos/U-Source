61b5703d47da4fe195833d80642b63613b0a0e1b
Colanatorsos <alibekmah527@gmail.com>
2023-09-16 22:50:11 +0600

    add fixes to contacts and Я устал

--- src/app/Contacts/page.jsx
+++ src/app/Contacts/page.jsx
@@ -1,29 +1,58 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import styles from './Contacts.module.css'
+import Image from 'next/image'
 
 const page = () => {
+    const [isButtonClicked, setIsButtonClicked] = useState(false);
+    const [buttonText, setButtonText] = useState('Отправить');
+
+    const handleButtonClick = () => {
+        setIsButtonClicked(true);
+        setButtonText('Отправлено');
+    };
+
     return (
         <div>
-            <section className={styles.section} >
+            <section className={styles.section}>
                 <div className={styles.container}>
-                    <div className={styles.block} >
-                        <h1 className={styles.title} >Свяжитесь с нами</h1>
-                        <div className={styles.card} >
+                    <div className={styles.block}>
+                        <h1 className={styles.title}>Свяжитесь с нами</h1>
+                        <div className={styles.card}>
                             <div className={styles.text}>
                                 Мы всегда готовы ответить на ваши вопросы и предоставить дополнительную информацию!
                             </div>
                             <form className={styles.form} action="">
-                                <input type="text" className={styles.input} placeholder='Имя' />
-                                <input type="text" className={styles.input} Required placeholder='Email' />
-                                <input type="text" className={styles.input} placeholder='Сообщение' />
-                                <button className={styles.btn} >Отправить</button>
+                                <input type="text" className={styles.input} placeholder="Имя" />
+                                <input type="text" className={styles.input} required placeholder="Email" />
+                                <input type="text" className={styles.input} placeholder="Сообщение" />
+                                <button
+                                    type="button"
+                                    className={`${styles.btn} ${isButtonClicked ? styles.clicked : ''}`}
+                                    onClick={handleButtonClick}
+                                >
+                                    {buttonText}
+                                </button>
                             </form>
+                            <div className={styles.side} >
+                                <h2 className={styles.sideTitle} >Информация</h2>
+                                <ul>
+                                    <li>
+                                    </li>
+                                    <li>
+                                    </li>
+                                    <li>
+                                    </li>
+                                    <li>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
 export default page
